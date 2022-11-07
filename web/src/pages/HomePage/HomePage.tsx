ff2207f0e2fb87f28a4fe7f39b2d77455bcf66d4
Daniel Escoto <escotodanny62@gmail.com>
2022-11-07 15:34:30 -0800

    change logout button color

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -14,7 +14,7 @@ const HomePageButtons = ({ isAuthenticated, logout }) => {
         </Link>
         <button
           onClick={logout}
-          className="ml-4 rounded bg-green-500 py-2 px-4 font-bold text-white hover:bg-green-700"
+          className="rounded bg-red-500 py-2 px-4 font-bold text-white hover:bg-red-700 ml-4"
         >
           Logout
         </button>
