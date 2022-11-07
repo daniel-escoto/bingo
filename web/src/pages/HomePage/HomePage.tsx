773eff5b928b2f0717210beb987b45374b16a32f
Daniel Escoto <escotodanny62@gmail.com>
2022-11-07 15:32:18 -0800

    add dynamic buttons to home page

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,19 +1,58 @@
+import { useAuth } from '@redwoodjs/auth'
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
 
+const HomePageButtons = ({ isAuthenticated, logout }) => {
+  if (isAuthenticated) {
+    return (
+      <>
+        <Link
+          to={routes.adminUsers()}
+          className="rounded bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-700"
+        >
+          Manage Users
+        </Link>
+        <button
+          onClick={logout}
+          className="ml-4 rounded bg-green-500 py-2 px-4 font-bold text-white hover:bg-green-700"
+        >
+          Logout
+        </button>
+      </>
+    )
+  } else {
+    return (
+      <>
+        <Link
+          to={routes.signup()}
+          className="rounded bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-700"
+        >
+          Sign Up
+        </Link>
+        <Link
+          to={routes.login()}
+          className="ml-4 rounded bg-green-500 py-2 px-4 font-bold text-white hover:bg-green-700"
+        >
+          Log In
+        </Link>
+      </>
+    )
+  }
+}
+
 const HomePage = () => {
+  const { isAuthenticated, logOut } = useAuth()
+
   return (
     <>
       <MetaTags title="Home" description="Home page" />
 
-      <h1>HomePage</h1>
-      <p>
-        Find me in <code>./web/src/pages/HomePage/HomePage.tsx</code>
-      </p>
-      <p>
-        My default route is named <code>home</code>, link to me with `
-        <Link to={routes.home()}>Home</Link>`
-      </p>
+      <div className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-4xl font-bold">Welcome to Bingo</h1>
+        <div className="flex flex-row mt-4">
+          <HomePageButtons isAuthenticated={isAuthenticated} logout={logOut} />
+        </div>
+      </div>
     </>
   )
 }
